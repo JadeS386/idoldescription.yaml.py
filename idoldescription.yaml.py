3ab3282a1b321c5d457485d957ad2a5f4bf6e5a9
JadeS386 <95428386+JadeS386@users.noreply.github.com>
2022-11-30 14:23:12 +0000

    Add files via upload

---
+++ idoldescription.yaml.py
@@ -0,0 +1,14 @@
+---
+# This is the biography of my idol
+Idol: Evanna Lynch
+Age: 31
+Name: Evanna Patricia Lynch
+Height: 5'2'' (1.57 m)
+Hair color: blonde
+Gender: female
+Career: actress, writer, vegan activist and podcaster
+Place of birth: Termonfeckin, Ireland
+Zodiac sign: Leo
+Birth month: 16 August
+Birth year: 1991
+Residence: London, United Kingdom
